970846a9a912d3ef39de5bca67553ee5c29d7745
SergSmith64 <sergsmith2008@yandex.ru>
2019-04-26 13:29:05 +0400

    commit_02_Task_completed

---
+++ Task_4_Factorial/Task_4_Factorial/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4_Factorial
+{
+    class Program
+    {
+        // ВЫЧИСЛЕНИЕ ФАКТОРИАЛА
+        public static long GetFact(int i)
+        {
+            if (i == 0)
+            {
+                return 1;
+            }
+            return i * GetFact(i - 1);
+        }
+        static void Main(string[] args)
+        {
+            // ВВОЖУ ЧИСЛО С КЛАВИАТУРЫ
+            Console.WriteLine("Введите ЦЕЛОЕ число - ");
+            int myNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("вы ввели число = " + myNum); // ПРОВЕРКА ВВОДА
+
+            // ВЫЗЫВАЮ ФУНКЦИЮ ВЫЧИСЛЕНИЕ ФАКТОРИАЛА
+            long myFact = GetFact(myNum);
+
+            // ВЫВОЖУ РЕЗУЛЬТАТ НА КОНСОЛЬ
+            // ДЕЛАЮ ИНТЕРПОЛЯЦИЮ СТРОК
+            Console.WriteLine($"Факториал из числа {myNum} равен = {myFact}");
+
+            Console.ReadKey();
+        }
+    }
+}
