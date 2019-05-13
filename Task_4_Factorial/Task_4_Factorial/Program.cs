e0468b545f19f0e8b8013ebf4cc36d6d80239bd8
SergSmith64 <sergsmith2008@yandex.ru>
2019-05-14 00:54:50 +0400

    commit_04_Task_Patching-2

--- Task_4_Factorial/Task_4_Factorial/Program.cs
+++ Task_4_Factorial/Task_4_Factorial/Program.cs
@@ -1,56 +1,67 @@
-﻿    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-     
-    namespace Task_4_Factorial
-    {
-        class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Task_4_Factorial
+{
+	class Program
+	{
+        // ВЫЧИСЛЕНИЕ ФАКТОРИАЛА
+        static int GetFact(int i)
+		{
+			int result;
+			// Факториал от НУЛЯ равен ЕДИНИЦЫ (принимается в качестве соглашения)
+			if (i == 0) return 1;
+			if (i == 1) return 1;
+			result = GetFact(i - 1) * i;
+			return result;
+		}
+        public void CheckInput()
         {
-            // ВЫЧИСЛЕНИЕ ФАКТОРИАЛА
-            static int GetFact(int i)
+            int i;
+            // ВВОЖУ ЧИСЛО С КЛАВИАТУРЫ
+            Console.WriteLine("\nВведите ЦЕЛОЕ ПОЛОЖИТЕЛЬНОЕ число : ");
+            try
             {
-                int result;
-                // Факториал от НУЛЯ равен ЕДИНИЦЫ (принимается в качестве соглашения)
-                if (i == 0) return 1;
-                if (i == 1) return 1;
-
-                result = GetFact(i - 1) * i;
-                return result;
-            }
-            static void Main(string[] args)
-            {
-                int i;
-                label1:
-                // ВВОЖУ ЧИСЛО С КЛАВИАТУРЫ
-                Console.WriteLine("Введите ЦЕЛОЕ ПОЛОЖИТЕЛЬНОЕ число : ");
-     
-                try 
-                {
                 i = int.Parse(Console.ReadLine());
-                    if (i < 0)
+                if (i >= 0)
+                {
+                    if (i <= 16)
                     {
-                        throw new Exception("вы ввели отрициательное число");
+                        Console.WriteLine("Факториал из {0}! = {1}", i, GetFact(i));
                     }
-                    else 
+                    else
                     {
-                    Console.WriteLine("Факториал из {0}! = {1}", i, GetFact(i));
+                        // _______ ЗДЕСЬ Я ДУМАЮ ВОЗНИКАЕТ ПЕРЕПОЛНЕНИЕ ПАМЯТИ _____________________________________________________
+                        Console.WriteLine("Факториал БОЛЬШЕ 16 вызывает переполнение памяти. Пожалуйста введите число от 0 до 16! ");
+                        CheckInput();
                     }
                     
-                }
-                catch(FormatException)
+                } 
+                else
                 {
-                    Console.WriteLine("Вы ввели НЕ число. Пожалуйста повторите! ");
-                    goto label1;
+                    Console.WriteLine("Вы ввели НЕкорректное число. Пожалуйста повторите! ");
+                    CheckInput();
                 }
-                catch(Exception) 
-                {
-                    Console.WriteLine("Вы ввели отрицательное число. Пожалуйста повторите! ");
-                    goto label1;
-                }
-     
-                Console.ReadKey();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Вы ввели НЕ число. Пожалуйста повторите! ");
+                CheckInput();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Вы ввели НЕкорректное число. Пожалуйста повторите! ");
+                CheckInput();
             }
         }
-    }
+        static void Main(string[] args)
+		{
+            Program f1 = new Program();
+            f1.CheckInput();
+            Console.ReadKey();
+		}
+	}
+}
